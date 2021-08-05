7b92864c4c0850d1a74cff34304c8bb1f0f3e698
amignon <amignon@gmail.com>
2021-08-05 17:34:04 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
-function App() {
+import { cache } from './cache';
+
+import EstiloGlobal from './componentes/EstiloGlobal';
+import Paginas from './paginas'
+
+const uri = process.env.REACT_APP_API_URI;
+const httpLink = createHttpLink({ uri });
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+// configura o Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <EstiloGlobal />
+      <Paginas />
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
